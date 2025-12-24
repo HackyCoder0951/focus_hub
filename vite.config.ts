025c058b67a17e08dc36763b40d205bc665ab94a
hackycoder0951 <jignesh.vision.17@gmail.com>
2025-12-24 20:35:48 +0530

    Comment out server configuration in vite.config.ts for future reference

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  /*server: {
     host: "::",
     port: 5173,
     proxy: {
       '/api': 'http://localhost:8080'
     }
-  },
+  },*/
   plugins: [
     react(),
   ],
